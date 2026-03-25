b0a02935b8e5edc95395757c45e0a756cb1c1c41
Xiang511 <toby91511@gmail.com>
2026-03-25 16:54:19 +0800

    feat: 權限管理 ViewModels

---
+++ Models/ViewModels/UserPermissionViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PawsPort.Models.ViewModels
+{
+    public class UserPermissionViewModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string SystemName { get; set; }
+        public string RoleName { get; set; }
+        public DateTime? AssignedAt { get; set; }
+    }
+}

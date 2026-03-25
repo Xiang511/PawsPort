0f549d3ee572df382239ab615f0a1b77c27845d4
Ellie <tswork0329@gmail.com>
2026-03-25 11:24:47 +0800

    feat: 新增活動管理，修改_layout上的頁面連結

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -11,7 +11,7 @@ namespace PawsPort.Controllers
         //    return View();
         //}
 
-        public IActionResult List(KeywordViewModel vm)
+        public IActionResult ArticleList(KeywordViewModel vm) //貼文管理頁面
         {
             PetDbContext db = new PetDbContext();
 
@@ -22,10 +22,28 @@ namespace PawsPort.Controllers
             }
             else
             {
-                datas = db.Articles.Where(a => a.IsExist && (a.Title.Contains(vm.txtKeyword) || a.Content.Contains(vm.txtKeyword))).ToList(); //根據搜尋條件查詢文章
+                datas = db.Articles.Where(a => a.IsExist && (a.Title.Contains(vm.txtKeyword)
+                || a.Content.Contains(vm.txtKeyword))).ToList(); //根據搜尋條件查詢文章
             }
 
             return View(datas);
         }
+
+        public IActionResult Event(KeywordViewModel vm) //活動管理頁面
+        {
+            PetDbContext db = new PetDbContext();
+            vm.CategoryId = 1; //假設活動的CategoryId為1
+
+            IEnumerable<Article> datas = null; //宣告一個變數來存放查詢結果
+            datas = db.Articles.Where(a => a.IsExist 
+            && (a.CategoryId == vm.CategoryId) //篩選出活動類別的文章
+            || (a.Title.Contains(vm.txtKeyword) 
+            || a.Content.Contains(vm.txtKeyword))
+                 ); //根據搜尋條件查詢文章
+
+            return View(datas);
+
+        }
+
     }
 }
--- ViewModels/KeywordViewModel.cs
+++ ViewModels/KeywordViewModel.cs
@@ -3,5 +3,6 @@
     public class KeywordViewModel
     {
         public string txtKeyword { get; set; }
+        public int CategoryId { get; set; }
     }
 }

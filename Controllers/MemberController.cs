6c0ff7793301eb081639a947519a8a75f93fa2f8
Xiang511 <toby91511@gmail.com>
2026-03-25 10:47:27 +0800

    feat: MemberController

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PawsPort.Models;
+
+namespace PawsPort.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult List()
+        {
+            PetDbContext db = new PetDbContext();
+            var p = db.UserTables.ToList();
+            return View(p);
+        }
+    }
+}

bcc31a7b71e5110c30285ff33134b507b700f64d
Ellie <tswork0329@gmail.com>
2026-03-25 12:09:56 +0800

    feat: 新增圖片管理，修改KeywordViewModel

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -17,7 +17,7 @@ namespace PawsPort.Controllers
 
             IEnumerable<Category> datas = null; //宣告一個變數來存放查詢結果
 
-            if (string.IsNullOrEmpty(vm.txtKeyword))
+            if (string.IsNullOrEmpty(vm.txtCategoryId.ToString()))
             {
                 datas = db.Categories.Where(a => a.IsExist).ToList(); //查詢所有存在的類別
             }
@@ -52,19 +52,35 @@ namespace PawsPort.Controllers
         }
 
 
+        public IActionResult ArticleImageList(KeywordViewModel vm)
+        {
+            PetDbContext db = new PetDbContext();
 
+            IEnumerable<ArticleImage> datas = null; //宣告一個變數來存放查詢結果
+            if (string.IsNullOrEmpty(vm.txtArticleId.ToString()))
+            {
+                datas = db.ArticleImages.Where(a => a.IsExist).ToList(); //查詢所有存在的文章圖片
+            }
+            else
+            {
+                datas = db.ArticleImages.Where(a => a.IsExist 
+                && (a.ArticleId == vm.txtArticleId
+                )).ToList(); //根據搜尋條件查詢文章圖片
+            }
+            return View(datas);
+        }
 
 
 
         public IActionResult EventList(KeywordViewModel vm) //活動管理頁面
         {
             PetDbContext db = new PetDbContext();
-            vm.CategoryId = 1; //假設活動的CategoryId為1
+            vm.txtCategoryId = 1; //假設活動的CategoryId為1
 
             IEnumerable<Article> datas = null; //宣告一個變數來存放查詢結果
 
             datas = db.Articles.Where(a => a.IsExist
-            && (a.CategoryId == vm.CategoryId) //篩選出活動類別的文章
+            && (a.CategoryId == vm.txtCategoryId) //篩選出活動類別的文章
             || (a.Title.Contains(vm.txtKeyword)
             || a.Content.Contains(vm.txtKeyword))
             || a.EventLocation.Contains(vm.txtKeyword)
--- ViewModels/KeywordViewModel.cs
+++ ViewModels/KeywordViewModel.cs
@@ -3,6 +3,7 @@
     public class KeywordViewModel
     {
         public string txtKeyword { get; set; }
-        public int CategoryId { get; set; }
+        public int txtCategoryId { get; set; }
+        public int txtArticleId { get; set; }
     }
 }

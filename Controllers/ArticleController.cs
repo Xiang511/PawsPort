1379c02e5fb054ad40d99c1471a2848495ec40d9
Ellie <tswork0329@gmail.com>
2026-03-25 11:49:45 +0800

    feat: 新增貼文分類管理，修正_layout裡的頁面連結(改用asp-controller/asp-action)

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -11,6 +11,28 @@ namespace PawsPort.Controllers
         //    return View();
         //}
 
+        public IActionResult CategoryList(KeywordViewModel vm)
+        {
+            PetDbContext db = new PetDbContext();
+
+            IEnumerable<Category> datas = null; //宣告一個變數來存放查詢結果
+
+            if (string.IsNullOrEmpty(vm.txtKeyword))
+            {
+                datas = db.Categories.Where(a => a.IsExist).ToList(); //查詢所有存在的類別
+            }
+            else
+            {
+                datas = db.Categories.Where(a => a.IsExist
+                && (a.CategoryName.Contains(vm.txtKeyword)
+                )).ToList(); //根據搜尋條件查詢類別
+            }
+
+            return View(datas);
+        }
+
+
+
         public IActionResult ArticleList(KeywordViewModel vm) //貼文管理頁面
         {
             PetDbContext db = new PetDbContext();
@@ -29,16 +51,23 @@ namespace PawsPort.Controllers
             return View(datas);
         }
 
-        public IActionResult Event(KeywordViewModel vm) //活動管理頁面
+
+
+
+
+
+        public IActionResult EventList(KeywordViewModel vm) //活動管理頁面
         {
             PetDbContext db = new PetDbContext();
             vm.CategoryId = 1; //假設活動的CategoryId為1
 
             IEnumerable<Article> datas = null; //宣告一個變數來存放查詢結果
-            datas = db.Articles.Where(a => a.IsExist 
+
+            datas = db.Articles.Where(a => a.IsExist
             && (a.CategoryId == vm.CategoryId) //篩選出活動類別的文章
-            || (a.Title.Contains(vm.txtKeyword) 
+            || (a.Title.Contains(vm.txtKeyword)
             || a.Content.Contains(vm.txtKeyword))
+            || a.EventLocation.Contains(vm.txtKeyword)
                  ); //根據搜尋條件查詢文章
 
             return View(datas);

5bafa574c1095791895bd1f037c89bbb02dddfe6
Xiang511 <toby91511@gmail.com>
2026-03-25 16:53:14 +0800

    feat: 權限管理 Controller

---
+++ Controllers/PermissionsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PawsPort.Models;
+using PawsPort.Models.ViewModels;
+
+namespace PawsPort.Controllers
+{
+    public class PermissionsController : Controller
+    {
+        public IActionResult List()
+        {
+            PetDbContext db = new PetDbContext();
+            var userPermissions = from u in db.UserTables
+                                  join usr in db.UserSystemRoles on u.UserId equals usr.UserId
+                                  join s in db.SystemTables on usr.SystemId equals s.SystemId
+                                  join r in db.RoleTables on usr.RoleId equals r.RoleId
+                                  // 可加入過濾條件，例如查詢特定使用者 ID
+                                  // where u.UserId == 1 
+                                  select new UserPermissionViewModel // <--- 必須指定類別名稱
+                                  {
+                                      UserId = u.UserId,
+                                      UserName = u.Name,
+                                      SystemName = s.SystemName,
+                                      RoleName = r.RoleName,
+                                      AssignedAt = usr.AssignedAt
+                                  };
+            return View(userPermissions);
+        }
+    }
+}

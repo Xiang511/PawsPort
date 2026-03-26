10dd85ff84d875a0134d16c74ec522478fd65dda
Xiang511 <toby91511@gmail.com>
2026-03-26 14:03:15 +0800

    fix: 題庫無法新增問題

--- Controllers/QuestionsController.cs
+++ Controllers/QuestionsController.cs
@@ -22,6 +22,9 @@ namespace PawsPort.Controllers
         public IActionResult Create(GameContent G)
         {
             PetDbContext db = new PetDbContext();
+
+            var count = db.GameContents.Count();
+            G.GameId = count + 1;
             Debug.WriteLine($"Create GameContent id : {G.GameId}");
 
             db.GameContents.Add(G);

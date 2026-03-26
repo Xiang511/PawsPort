cd3a4f4c9629c3094b4c238a91c17601e68d7c6c
Teddie <sky68452@gmail.com>
2026-03-26 17:25:48 +0800

    feat: 新增Edit功能

--- Controllers/QuestionsController.cs
+++ Controllers/QuestionsController.cs
@@ -26,22 +26,37 @@ namespace PawsPort.Controllers
             db.SaveChanges();
             return RedirectToAction("List");
         }
- 
-        public IActionResult Create2()
-        {
-            return View();
 
-        }
 
-        [HttpPost]
-        public IActionResult Create2(GameContent G)
+        // 編輯題目 - GET 方法
+        public IActionResult Edit(int? id)
         {
             PetDbContext db = new PetDbContext();
-            db.GameContents.Add(G);
-            db.SaveChanges();
-            return RedirectToAction("List");
+            GameContent game = db.GameContents.FirstOrDefault(game => game.GameId == id);
+            if (game == null)
+                return RedirectToAction("List");
+            return View(game);
         }
 
+        // 編輯題目 - POST 方法
+        [HttpPost]
+        public IActionResult Edit(GameContent G)
+        {
+            PetDbContext db = new PetDbContext();
+            GameContent game = db.GameContents.FirstOrDefault(game => game.GameId == G.GameId);
+            if (game != null)
+            {
+                game.GameName = G.GameName;
+                game.Questions = G.Questions;
+                game.AnswersDetail = G.AnswersDetail;
+                game.Answers = G.Answers;
+                game.IsActive = G.IsActive;
+                game.Rewards = G.Rewards;
+                game.Type = G.Type;
+                db.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
 
     }
 }

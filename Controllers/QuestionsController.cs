a5b545cf6351760a82f82883c1a11db11d16a260
Teddie <sky68452@gmail.com>
2026-03-26 13:44:40 +0800

    feat: 圖庫新增

---
+++ Controllers/QuestionsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PawsPort.Models;
+using System.Diagnostics;
+
+namespace PawsPort.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        // 題庫列表
+        public IActionResult List()
+        {
+            PetDbContext db = new PetDbContext();
+            var GameList = db.GameContents.ToList();
+            return View(GameList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(GameContent G)
+        {
+            PetDbContext db = new PetDbContext();
+            Debug.WriteLine($"Create GameContent id : {G.GameId}");
+
+            db.GameContents.Add(G);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+ 
+        public IActionResult Create2()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Create2(GameContent G)
+        {
+            PetDbContext db = new PetDbContext();
+            db.GameContents.Add(G);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+
+    }
+}
--- Models/GameContent.cs
+++ Models/GameContent.cs
@@ -1,23 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PawsPort.Models;
 
-public partial class GameContent
+public class GameContent
 {
     public int GameId { get; set; }
-
+    [DisplayName("題目類型")]
     public string GameName { get; set; } = null!;
-
+    [DisplayName("題目內容")]
     public string Questions { get; set; } = null!;
-
+    [DisplayName("詳細解答")]
     public string AnswersDetail { get; set; } = null!;
-
+    [DisplayName("是否正確")]
     public bool Answers { get; set; }
-
+    [DisplayName("是否啟用")]
     public bool IsActive { get; set; }
-
+    [DisplayName("題目獎勵")]
     public int Rewards { get; set; }
-
+    [DisplayName("遊戲類型")]
     public string? Type { get; set; }
 }

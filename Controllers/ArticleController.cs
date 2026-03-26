b00cf5a04f7667e61f05b0905f8390632d93f5d4
Ellie <tswork0329@gmail.com>
2026-03-26 13:31:45 +0800

    refactor: 拆分category到新控制器

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -11,29 +11,7 @@ namespace PawsPort.Controllers
         //    return View();
         //}
 
-        public IActionResult CategoryList(KeywordViewModel vm)
-        {
-            PetDbContext db = new PetDbContext();
-
-            IEnumerable<Category> datas = null; //宣告一個變數來存放查詢結果
-
-            if (string.IsNullOrEmpty(vm.txtKeyword))
-            {
-                datas = from p in db.Categories
-                        where p.IsExist
-                        select p;
-            }
-            else
-            {
-                datas = db.Categories.Where(a => a.IsExist
-                && (a.CategoryName.Contains(vm.txtKeyword)
-                )); //根據搜尋條件查詢類別
-            }
-
-            return View(datas);
-        }
-
-
+       
         
         public IActionResult ArticleList(KeywordViewModel vm) //貼文管理頁面
         {
---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PawsPort.Models;
+using PawsPort.ViewModels;
+
+namespace PawsPort.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        public IActionResult CategoryList(KeywordViewModel vm)
+        {
+            PetDbContext db = new PetDbContext();
+
+            IEnumerable<Category> datas = null; //宣告一個變數來存放查詢結果
+
+            if (string.IsNullOrEmpty(vm.txtKeyword))
+            {
+                datas = from p in db.Categories
+                        where p.IsExist
+                        select p;
+            }
+            else
+            {
+                datas = db.Categories.Where(a => a.IsExist
+                && (a.CategoryName.Contains(vm.txtKeyword)
+                )); //根據搜尋條件查詢類別
+            }
+
+            return View(datas);
+        }
+    }
+}

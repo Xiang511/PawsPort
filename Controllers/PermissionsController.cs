4edf50cb591171cd6da0279755447c78a0164f0e
Xiang511 <toby91511@gmail.com>
2026-03-27 14:49:45 +0800

    feat: 違規名單 CRUD

--- Controllers/PermissionsController.cs
+++ Controllers/PermissionsController.cs
@@ -91,5 +91,94 @@ namespace PawsPort.Controllers
 
             return RedirectToAction("List");
         }
+
+
+
+        public IActionResult BlockList()
+        {
+            PetDbContext db = new PetDbContext();
+            var userPermissions = from u in db.UserTables
+                                  join usr in db.UserSystemRoles on u.UserId equals usr.UserId
+                                  join s in db.SystemTables on usr.SystemId equals s.SystemId
+                                  join r in db.RoleTables on usr.RoleId equals r.RoleId
+                                  where r.RoleId == 3
+                                  // 可加入過濾條件，例如查詢特定使用者 ID
+                                  // where u.UserId == 1 
+                                  select new UserPermissionViewModel // <--- 必須指定類別名稱
+                                  {
+                                      UserId = u.UserId,
+                                      UserName = u.Name,
+                                      SystemName = s.SystemName,
+                                      RoleName = r.RoleName,
+                                      AssignedAt = usr.AssignedAt,
+                                      MappingId = usr.MappingId
+                                  };
+            return View(userPermissions);
+        }
+
+
+        public IActionResult BlockListCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BlockListCreate(UserSystemRole US)
+        {
+            PetDbContext db = new PetDbContext();
+            db.UserSystemRoles.Add(US);
+            db.SaveChanges();
+            return RedirectToAction("BlockList");
+        }
+
+        public IActionResult BlockListEdit(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("BlockList");
+
+            PetDbContext db = new PetDbContext();
+            UserSystemRole x = db.UserSystemRoles.Where(m => m.UserId == id).FirstOrDefault();
+
+            if (x == null)
+                return RedirectToAction("BlockList");
+
+            return View(x);
+        }
+
+        [HttpPost]
+        public IActionResult BlockListEdit(UserSystemRole uiUserSystemRole)
+        {
+            PetDbContext db = new PetDbContext();
+            UserSystemRole dbUserSystemRole = db.UserSystemRoles.Where(m => m.UserId == uiUserSystemRole.UserId).FirstOrDefault();
+
+            if (dbUserSystemRole != null)
+            {
+                dbUserSystemRole.SystemId = uiUserSystemRole.SystemId;
+                dbUserSystemRole.RoleId = uiUserSystemRole.RoleId;
+                dbUserSystemRole.AssignedAt = uiUserSystemRole.AssignedAt;
+                dbUserSystemRole.UserId = uiUserSystemRole.UserId;
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("BlockList");
+        }
+
+        public IActionResult BlockListDelete(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("BlockList");
+
+            PetDbContext db = new PetDbContext();
+            UserSystemRole x = db.UserSystemRoles.Where(m => m.MappingId == id).FirstOrDefault();
+
+            if (x != null)
+            {
+                db.UserSystemRoles.Remove(x);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("BlockList");
+        }
     }
 }

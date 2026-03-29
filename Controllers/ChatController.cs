45c6a13e550f629c3d623366c345030d35095abe
Ellie <tswork0329@gmail.com>
2026-03-29 12:09:58 +0800

    feat : 新增ChatroomListViewModel

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -4,9 +4,26 @@ namespace PawsPort.Controllers
 {
     public class ChatController : Controller
     {
-        public IActionResult Index()
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        public IActionResult ChatroomList()
         {
+
+            //要有一個頁面可以搜尋一個會員底下所有的聊天室 =ChatroomList
+            //觀察有沒有會員檢舉某些聊天室的訊息，並且查看該訊息是什麼
+            //要有顯示區顯示該會員的近期(一周)聊天訊息數量，用來篩選是不是有機器人
+            //要有一個顯示區顯示全站近期(一周)的聊天室和聊天訊息數量，用來篩選是不是有機器人
+
+
+            //要有一個功能可以將該會員的聊天功能暫時停權，並暫時封存所有他底下的聊天室 =ChatroomDelete
+
+
             return View();
         }
+
     }
 }
---
+++ ViewModels/ChatroomListViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace PawsPort.ViewModels
+{
+    public class ChatroomListViewModel
+    {
+
+        // 全站儀表板數據
+        public int TotalMessages24h { get; set; }
+        public int ActiveRoomsCount { get; set; }
+        public int SuspiciousUserCount { get; set; } //疑似異常帳號
+
+        // 搜尋參數
+        public string TxtKeyword { get; set; }
+
+        // 聊天室清單 (小包包)
+        public List<ChatroomItemViewModel> Chatrooms { get; set; }
+    }
+
+    public class ChatroomItemViewModel
+    {
+        public int ChatroomId { get; set; }
+        public string RoomName { get; set; }
+        public int MemberCount { get; set; }
+        
+        // 新增：如果是 1對1，直接顯示對方是誰
+        public string OtherSideName { get; set; }
+        public DateTime LastMessageTime { get; set; }
+        public int MsgCount24h { get; set; }
+
+        // 雖然資料庫沒欄位，但我們可以透過邏輯判斷是否「異常熱鬧」
+        public bool IsHot => MsgCount24h > 100;
+    }
+
+}

2c6b0c6a2e0a8de4b99ae6f590c38e60d7f4d1c2
Xiang511 <toby91511@gmail.com>
2026-04-07 11:47:44 +0800

    feat: 新增 Serilog 簡易日誌功能

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PawsPort.Models;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,21 @@ builder.Services.AddDbContext<PetDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllersWithViews();
+
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information() // 設定最低紀錄等級
+    .Enrich.FromLogContext()    // 記錄更多上下文資訊
+    .WriteTo.Debug()
+    .WriteTo.File("logs/all-log-.txt", rollingInterval: RollingInterval.Day) // 每天產生一個檔案
+    .WriteTo.Logger(lc => lc
+    .Filter.ByIncludingOnly(e => e.Level >= LogEventLevel.Error)
+    .WriteTo.File("logs/only-errors-.txt", rollingInterval: RollingInterval.Day))
+    //.WriteTo.Seq("http://localhost:5341") // 將資料送到 Seq 監控軟體
+    .CreateLogger();
+
+builder.Host.UseSerilog(); 
+
 
 var app = builder.Build();
 

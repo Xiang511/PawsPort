dea7fce8b004a3e70572522d9d4dd076f0af3e79
Xiang511 <toby91511@gmail.com>
2026-04-11 16:31:23 +0800

    feat: 將資料傳送到視覺化平台 seq

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .Enrich.FromLogContext()
     .WriteTo.Debug()
+    .WriteTo.Seq("http://localhost:5341")
 
     // 1. 一般日誌：每天分檔 + 大小限制 (10MB)
     .WriteTo.File(

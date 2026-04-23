e82e954fad4b6950f033f4f9a55cb1bd82413744
Xiang511 <toby91511@gmail.com>
2026-04-24 01:03:49 +0800

    feat: 新增 ApiControllerBase 和 ApiResponse 類別以支援 API 回應格式

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PawsPort.Responses;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PawsPort.Controllers
+{
+    [ApiController]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+         // (200 OK)
+        protected IActionResult Success<T>(T data, string message = "Success", int statusCode = 200)
+        {
+            return StatusCode(statusCode, ApiResponse<T>.Ok(data, message));
+        }
+
+        // (400 401 403 404 405)
+        protected IActionResult Failure(string code, string message, int statusCode = 400)
+        {
+            var result = ApiResponse<Array>.Fail(code, message);
+            return StatusCode(statusCode, result);
+        }
+
+        // 204 使用原本的 NoContent()
+    }
+}
---
+++ Responses/ApiResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace PawsPort.Responses
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+
+        //成功回傳
+        public static ApiResponse<T> Ok(T data, string message = "Success")
+            => new() { Success = true, Code = "SUCCESS", Message = message, Data = data };
+
+        //失敗回傳
+        public static ApiResponse<object> Fail(string code, string message)
+            => new()
+            {
+                Success = false,
+                Code = code,
+                Message = message,
+                Data = new string[0] // 強制回傳 JSON 中的 []
+            };
+    }
+}

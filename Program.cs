ff82bb13a86f2449e05662093b737982322dd95d
Teddie <sky68452@gmail.com>
2026-05-04 14:48:56 +0800

    fix: 修改Program.cs 以處理CORS錯誤

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// 處理CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowVueApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // 這是你的 Vue 預設埠號
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 
 
 // 註冊資料庫連線
@@ -183,6 +195,8 @@ app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 app.UseRouting();
+// 啟用 CORS
+app.UseCors("AllowVueApp");
 
 app.UseAuthorization();
 

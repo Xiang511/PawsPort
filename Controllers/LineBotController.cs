823aee680caa6bfd941176a4a7126b0d47253306
Phoebe <phoebeph53@gmail.com>
2026-05-05 15:57:50 +0800

    refactor: 將 LineBotController中的權限功能拆成 LineBotService

--- Controllers/LineBotController.cs
+++ Controllers/LineBotController.cs
@@ -1,70 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using PawsPort.Models;
-using System.Linq;
+using PawsPort.Dtos;
+using PawsPort.Services;
 
 namespace PawsPort.Controllers
 {
-    public class LineBotController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+
+
+    public class LineBotController : ApiControllerBase
     {
-        public ActionResult List()
-        {
-            PetDbContext db = new PetDbContext();
+        private readonly LineBotService _lineBotService;
 
-            var typeOrder = new List<string>
+        public LineBotController(LineBotService lineBotService)
+        {
+            _lineBotService = lineBotService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            var result = await _lineBotService.GetAllMessagesAsync();
+            return Success(result, "取得 LineBot 訊息列表成功", 200);
+        }
+
+
+        [HttpPost("{id}/reply")]
+        public async Task<IActionResult> Reply(int id, LineBotReplyDTO dto)
+        {
+            if (!ModelState.IsValid)
             {
-                    "認養",
-                    "醫療",
-                    "帳號",
-                    "系統",
-                    "遊戲",
-                    "其他"
-            };
+                return Failure("VALIDATION_ERROR", "請填寫回覆內容", 400);
+            }
 
-            var listFromDb = db.LineBots.ToList();
+            var isSuccess = await _lineBotService.ReplyMessageAsync(id, dto);
 
-            var messages = listFromDb
-        .OrderBy(b =>
-        {
-
-            int index = typeOrder.IndexOf(b.QuestionType);
-
-            return index == -1 ? 99 : index;
-        })
-        .ThenByDescending(b => b.ChatDate)
-        .ToList();
+            if (!isSuccess)
+            {
+                return Failure("MESSAGE_NOT_FOUND", "找不到該筆訊息，無法回覆", 404);
+            }
 
 
-            return View(messages);
-        }
-
-
-        public ActionResult Reply(int? id)
-        {
-            if (id == null) return RedirectToAction("List");
-
-            PetDbContext db = new PetDbContext();
-
-            var message = db.LineBots.FirstOrDefault(b => b.Id == id);
-
-            if (message == null) return RedirectToAction("List");
-
-            return View(message);
-        }
-
-
-        [HttpPost]
-        public ActionResult Reply(int Id, string ReplyText)
-        {
-            // 【未來擴充區】
-            // 因為目前資料庫 LineBot 表格沒有 ReplyContent 欄位
-            // 要串接 LINE Messaging API，就是寫在這裡！
-            // 例如： LineApi.PushMessage(UserId, ReplyText);
-
-            //先寫假回覆待串api
-            TempData["SuccessMessage"] = "✅ 成功！回覆訊息已透過 LINE Bot 傳送給該名使用者。";
-
-
-            return RedirectToAction("List");
+            return Success<object>(null, "✅ 成功！回覆訊息已透過 LINE Bot 傳送給該名使用者。", 200);
         }
     }
 }
---
+++ Services/LineBotService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PawsPort.Dtos;
+using PawsPort.Models;
+
+namespace PawsPort.Services
+{
+    public class LineBotService
+    {
+        private readonly PetDbContext _db;
+
+        public LineBotService(PetDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<List<LineBotDTO>> GetAllMessagesAsync()
+        {
+            var typeOrder = new List<string>
+            {
+                "認養", "醫療", "帳號", "系統", "遊戲", "其他"
+            };
+
+            var listFromDb = await _db.LineBots.ToListAsync();
+
+            return listFromDb
+                .OrderBy(b =>
+                {
+                    int index = typeOrder.IndexOf(b.QuestionType);
+                    return index == -1 ? 99 : index;
+                })
+                .ThenByDescending(b => b.ChatDate)
+                .Select(b => new LineBotDTO
+                {
+                    Id = b.Id,
+                    UserId = b.UserId,
+                    ChatDate = b.ChatDate,
+                    QuestionType = b.QuestionType,
+                    ChiefComplaint = b.ChiefComplaint,
+                    ChatContent = b.ChatContent
+                })
+                .ToList();
+        }
+
+
+        public async Task<bool> ReplyMessageAsync(int id, LineBotReplyDTO dto)
+        {
+            var message = await _db.LineBots.FindAsync(id);
+            if (message == null) return false;
+
+            // 【未來擴充區】
+            // 未來要串接 LINE API 時，可以用 message.UserId 去推播給特定使用者
+            // LineApi.PushMessage(message.UserId.ToString(), dto.ReplyText);
+            // 等你們資料庫如果加了 ReplyContent 跟 ReplyDate 欄位，也可以在這裡存檔：
+            // message.ReplyContent = dto.ReplyText;
+            // message.ReplyDate = DateTime.Now;
+            // await _db.SaveChangesAsync();
+
+
+            // 目前先直接回傳 true 代表模擬傳送成功
+            return true;
+        }
+    }
+}

c8993b75f9f8bff25a0afb15cacce767f6bd2ca2
Xiang511 <toby91511@gmail.com>
2026-05-17 23:51:06 +0800

    feat: 新增 AuthService 與 LoginDTO

---
+++ Dtos/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace PawsPort.Dtos
+{
+    public class LoginDTO
+    {
+        public string UserEmail { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ Services/AuthService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using PawsPort.Models;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PawsPort.Services
+{
+    public class AuthService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly MemberProfileService _memberProfileService;
+        private readonly PetDbContext _context;
+
+        public AuthService(IConfiguration configuration, MemberProfileService memberProfileService, PetDbContext PetDbContext)
+        {
+            _configuration = configuration;
+            _memberProfileService = memberProfileService;
+            _context = PetDbContext;
+        }
+
+        public async Task<string> ValidateUser(string email,string password)
+        {
+
+            bool isExist = await _context.UserAuthTables
+               .AnyAsync(x => x.Email == email && x.Password == password);
+
+            if (isExist)
+            {
+                // 2. 產生 Token
+                var token = GenerateJwtToken(email);
+                return token;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+
+        public string GenerateJwtToken(string UserEmail)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]));
+
+            // 定義 Claims (聲明)，這些資訊會加密在 Token 中
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Email, UserEmail),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) // Token 唯一識別碼
+        };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(double.Parse(jwtSettings["ExpireMinutes"])),
+                Issuer = jwtSettings["Issuer"],
+                Audience = jwtSettings["Audience"],
+                SigningCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}

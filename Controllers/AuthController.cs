86fcebdcb7bbc3bf6344c1477229d89a8c2624c4
Xiang511 <toby91511@gmail.com>
2026-05-18 13:45:34 +0800

    feat: 實作 HttpOnly Cookie 功能

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
 using PawsPort.Dtos;
 using PawsPort.Models;
 using PawsPort.Services;
@@ -16,9 +17,11 @@ namespace PawsPort.Controllers
     public class AuthController : ApiControllerBase
     {
         private readonly AuthService _authService;
-        public AuthController(AuthService authService)
+        private readonly MemberProfileService _memberProfileService;
+        public AuthController(AuthService authService,MemberProfileService memberProfileService)
         {
             _authService = authService;
+            _memberProfileService = memberProfileService;
         }
 
         [HttpPost("login")]
@@ -29,13 +32,23 @@ namespace PawsPort.Controllers
                 return Failure("使用者不存在", "User_Not_Found", 404);
             }
             else
-            {
+            {   
+                var userInfo = await _memberProfileService.GetUserInfoByEmailAsync(model.UserEmail);
                 var token = await _authService.ValidateUser(model.UserEmail,model.Password);
 
                 if (!string.IsNullOrEmpty(token))
                 {
-                    
-                    return Success(new { Token = token });
+                    Response.Cookies.Append("X-Access-Token", token, new CookieOptions
+                    {
+                        HttpOnly = true, // 前端 JavaScript 無法讀取，防範 XSS
+                        Secure = Request.IsHttps, // 根據當前請求協議決定（生產環境應該是 HTTPS）
+                        SameSite = SameSiteMode.Lax, // 防範 CSRF 攻擊
+                        Path = "/",
+                        Expires = DateTimeOffset.UtcNow.AddHours(2)
+                    });
+
+                    Log.Debug("[AuthController] Login - 成功登入, Email: {Email}", model.UserEmail);
+                    return Success(new { Token = token, User = userInfo });
                 }
                 else
                 {
@@ -45,8 +58,33 @@ namespace PawsPort.Controllers
             }
             
         }
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            Log.Debug("[AuthController] Logout POST - Entry");
 
-        
+            // 檢查 Cookie 是否存在
+            if (Request.Cookies.ContainsKey("X-Access-Token"))
+            {
+                // 建立一個與登入時完全相同的設定檔（除了過期時間）
+                var cookieOptions = new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = Request.IsHttps, // 根據當前請求協議決定
+                    SameSite = SameSiteMode.Lax,
+                    Path = "/",
+                    Expires = DateTimeOffset.UtcNow.AddDays(-1)
+                };
+
+                // 刪除 Cookie
+                Response.Cookies.Delete("X-Access-Token", cookieOptions);
+                Log.Debug("[AuthController] Logout - Cookie 已刪除");
+            }
+
+            Log.Debug("[AuthController] Logout POST - Exit");
+            return Success("登出成功");
+        }
+
     }
 }
 

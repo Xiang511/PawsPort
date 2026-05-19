fa2f76bf19a24ae63831c5fd1a22bf21f2e2bf8d
Ellie <tswork0329@gmail.com>
2026-05-19 10:45:31 +0800

    feat : 新增取得所有分類 API

---
+++ Dtos/CategoryListDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace PawsPort.Dtos
+{
+    public class CategoryListDTO
+    {
+        public string ParentCategoryName { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
